f677e682380487412280744701691b6beb1ef4cb
Couves-alcatifa <miguel.alves01@hotmail.com>
2024-03-19 18:41:20 +0000

    script para compilar no rnl alterado

--- mpi/life3d_job.sh
+++ mpi/life3d_job.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#SBATCH --job-name=life3d-mpi
-#SBATCH --output=mpi_%j.out
-#SBATCH --error=mpi_%j.err
-#SBTACH --ntasks=16
-#SBTACH --ntasks-per-node=4
-#SBATCH --nodes=4
-
-ARGS=(10, 512, 0.4, 0) 
+#SBATCH --job-name=life3d-mpi-1000-g20-2node
+#SBATCH --output=mpi_%j-mpi-1000-g20-2node.out
+#SBATCH --error=mpi_%j-mpi-1000-g20-2node.err
+#SBTACH --ntasks=2
+#SBTACH --ntasks-per-node=1
+#SBATCH --nodes=2
+#SBATCH --cpus-per-task=4
+ARGS=(1000, 64, 0.4, 0)
 
 srun ./life3d-mpi ${ARGS[@]}
